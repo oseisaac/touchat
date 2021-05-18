3a7b2447c47409f6439fef331ce1da5f1dbec085
ahmedwab <52334883+ahmedwab@users.noreply.github.com>
2021-05-17 20:47:32 -0400

    Chat room updates

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express')
+/*
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+*/
+const express = require('express');
+const hbs = require('express-handlebars');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const hbs = require('express-handlebars');
-const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
-
+const bodyParser = require('body-parser');
+/*
 app.engine('hbs',hbs({
     extname:'hbs',
     defaultLayout:'layout',
@@ -16,38 +24,62 @@ app.set('view engine','hbs');
 app.use("/css",express.static(__dirname + '/public/css'))
 
 const urlencodeParser = bodyParser.urlencoded({extended:false})
+const jsonParser = bodyParser.json();
 
 
-app.get('/chatroom/:id', (req, res) => {
-  let id = req.params.id;
-  res.render('chatroom',{
-    title:id,
-    style:"/css/chat-style.css"
-    
-  })
-});
-app.get('/chatroom/', (req, res) => {
-  let id = req.params.id;
-  res.render('chatroom',{
-    title:id,
-    style:"/css/chat-style.css"
-    
-  })
-});
-app.get('/', (req, res) => {
-  res.render('index',{
-    title:'Touchat',
-    style:"/css/index-style.css"
-    
-  })
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+//GET methods
+app.get("/chatroom/:id",(req,res)=>{
+    let id = req.params.id;
+    res.render('chatroom',{
+        title:id,
+        style:"../css/chat-style.css"
+        
+    })
+})
+app.get("/chatroom",(req,res)=>{
+    let id = req.params.id;
+    res.render('chatroom',{
+        title:id,
+        style:"../css/chat-style.css"
+        
+    })
+})
+*/
+app.get('/test', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
   });
-});
+  
+  io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+      io.emit('chat message', msg);
+    });
+  });
+  
+/*
+app.get("/",(req,res)=>{
 
-http.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}/`);
-});
+    res.render('index',{
+        title:"Chatroom",
+        style:"css/index-style.css"
+        
+    })
+
+})
+
+//POST methods
+
+app.post("/chatroom",urlencodeParser,(req,res)=>{
+    let chatName = req.body.chatid
+    res.render('chatroom',{
+        title:chatName,
+        style:"css/chat-style.css"
+        
+    })
+
+})
+
+
+
+*/
+
+app.listen(port)
